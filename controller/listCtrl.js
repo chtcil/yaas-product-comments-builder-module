df0e23cdc050ca711469b3b5480937ee22d01732
Nemanja Popovic <nemanja.popovic01@sap.com>
2016-10-06 23:31:03 +0200

    Added ui for showing products and their comments

--- controller/listCtrl.js
+++ controller/listCtrl.js
@@ -1,11 +1,49 @@
 var defaultListApp = angular.module('defaultListApp', ['builder', 'builder_editors']);
-defaultListApp.controller('defaultListCtrl', ['$scope','Restangular', function($scope, Restangular){
-    $scope.items = ['item1','item2','item3','item4'];
-    Restangular.setBaseUrl('https://api.yaas.io/nemanja/product-comments/v1/');
-    Restangular.one('YOUR_ENDPOINT').getList().then(function(items){
-        $scope.items = items;
-    },function(error){
-        //handle error
-    });
+defaultListApp.controller('defaultListCtrl', ['$scope', '$http', function ($scope, $http) {
+    $scope.products = [];
 
+    console.log(Builder.currentProjectId);
+    console.log(Builder.accessToken);
+    var tenant = Builder.currentProjectId;
+    $scope.selectedProduct = {};
+    $scope.products = [];
+    $scope.comments = [];
+
+    $scope.getProducts = function () {
+        $http({
+            method: 'GET',
+            url: 'https://api.yaas.io/hybris/product/v2/' + tenant + '/products',
+            headers: {
+                'Authorization': 'Bearer ' + Builder.accessToken
+            }
+        }).then(function (response) {
+            $scope.products = response.data;
+        }, function (error) {
+
+        });
+    };
+
+    $scope.getComments = function (productId) {
+        $http({
+            method: 'GET',
+            url: 'https://api.yaas.io/nemanja/product-comments/v1/' + tenant + '/products/' + productId + '/comments',
+            headers: {
+                'Authorization': 'Bearer ' + Builder.accessToken
+            }
+        }).then(function (response) {
+            $scope.comments = response.data;
+        });
+    };
+
+    $scope.selectProduct = function(product){
+        $scope.selectedProduct = product;
+        $scope.comments = [];
+        $scope.getComments(product.id);
+    };
+
+    $scope.deleteComment = function(comment){
+        $scope.getComments($scope.selectedProduct.id);
+    };
+
+    $scope.getProducts();
 }]);
